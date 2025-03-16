3170a51444ad841b9181617a1b107fb9a2f7466b
Dave Shoemaker <dave@gvn.com>
2025-03-16 10:07:40 -0700

    Added check for command line args

--- dave.sh
+++ dave.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "This is a file conversion script"
 
+# Check for valid arguments
+if [  "$#" -lt 2 ]
+then
+    echo "Invalid number of arguments; provide an input filename"
+    exit 
+fi
+
 echo "Begin processing"
+
 
 DATA="${1}"
